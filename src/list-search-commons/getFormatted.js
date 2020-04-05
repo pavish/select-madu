e27b4c6b8debc304a4804547a6f0d21ad8378c06
pavish <pavishkumar.r@gmail.com>
2020-04-05 17:48:23 +0530

    Restructuring components - removing old files

--- src/list-search-commons/getFormatted.js
+++
@@ -1,6 +0,0 @@
-export default function(formatters, type, obj, textKey) {
-  if(formatters && typeof formatters[type] === "function") {
-    return formatters[type](obj);
-  }
-  return obj[textKey];
-}
--- src/list-search-commons/main.js
+++
@@ -1,9 +0,0 @@
-import fetchData from './fetchData.js';
-import getFormatted from './getFormatted.js';
-import listOpts from './list-opts/main.svelte';
-
-export default {
-  fetchData,
-  getFormatted,
-  listOpts
-}
